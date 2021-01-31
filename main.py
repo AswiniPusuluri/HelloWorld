40fcc9673733092cd58079dc1c3e4c921f2885b0
Aswini Pusuluri <aswinipusuluri@gmail.com>
2021-01-30 20:25:06 -0800

    My First Hello World Project

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+   # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+    #print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+"""a='Hello World'
+print(a)
+addition:
+a=10
+b=5
+print(a+b)
+
+
+
+for i in range(1,10):
+    print('1'*i)
+
+
+a='Aswini'
+for i in a:
+    print(i)"""
+
+print("Hello World")
