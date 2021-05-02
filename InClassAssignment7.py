ffd9e5f0625379728a7ecff659da272d068953a2
aswinipusuluri <aswinipusuluri@gmail.com>
2021-05-01 18:10:24 -0700

    In-ClassAssignment7

--- InClassAssignment7.py
+++ InClassAssignment7.py
@@ -4,7 +4,7 @@ import pandas as pd
 data=pd.read_csv("BL-Flickr-Images-Book-truncated50.csv")
 choose = ['London', 'LONDON']
 result = data[data['Place of Publication'].isin(choose)]
-finalresult=result.drop(['Identifier','Edition Statement','Contributors','Corporate Author',
-                'Corporate Contributors','Former owner','Engraver','Issuance type',
-             'Flickr URL','Shelfmarks'], axis=1)
+finalresult=result.drop(['Former owner','Engraver','Issuance type',
+             'Flickr URL','Shelfmarks','Identifier','Edition Statement','Contributors','Corporate Author',
+                'Corporate Contributors',], axis=1)
 finalresult.to_csv('BL-Flickr-Images-Book-truncated50_Output.csv')
